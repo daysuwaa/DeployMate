3d8fa6edaeb116b36b0776941fe7c3a18c5f97dc
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-22 12:48:15 +0100

    edited my font in tailwind config

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <h1 className='bg-gray-200 text-center h-[4rem] pt-3'>How to Commit & Deploy to Git </h1>
+      <h1 className='bg-gray-200 text-center h-[4rem] pt-3 text-black'>How to Commit & Deploy to Git </h1>
     </div>
   )
 }
