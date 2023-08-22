7e97940c3fefd6676d8b980eb3f0467ac0e27904
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-22 12:26:10 +0100

    First commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image1 from '../assets/step1.png'
+import Image2 from '../assets/step2.png'
+import Image3 from '../assets/step1.png'
+import Image4 from '../assets/step4.png'
+import Image5a from '../assets/step5.1.png'
+import Image5b from '../assets/step5.2.png'
+import Image6 from '../assets/step6.png'
+import Image7 from '../assets/step7.png'
+import Image8 from '../assets/step8.png'
+import Image9 from '../assets/step9.jpg'
+import Image10 from '../assets/step10.png'
+import Image11 from '../assets/step11.png'
+import Image12 from '../assets/Step12.png'
+import Image13 from '../assets/lasstep.jpg'
+
+
+const Body = () => {
+  return (
+    <div>Body</div>
+  )
+}
+
+export default Body
