5d7b83b999848e22a584980a7913fa83be67b0be
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-22 19:00:17 +0100

    added the images to the body code

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,24 +1,194 @@
 import React from 'react'
-// import Image1 from '../assets/step1.png'
-// import Image2 from '../assets/step2.png'
-// import Image3 from '../assets/step1.png'
-// import Image4 from '../assets/step4.png'
-// import Image5a from '../assets/step5.1.png'
-// import Image5b from '../assets/step5.2.png'
-// import Image6 from '../assets/step6.png'
-// import Image7 from '../assets/step7.png'
-// import Image8 from '../assets/step8.png'
-// import Image9 from '../assets/step9.jpg'
-// import Image10 from '../assets/step10.png'
-// import Image11 from '../assets/step11.png'
-// import Image12 from '../assets/Step12.png'
-// import Image13 from '../assets/lasstep.jpg'
+import Image1 from '../assets/step1.png'
+import Image2 from '../assets/step2.png'
+import Image3 from '../assets/step3.png'
+import Image4 from '../assets/step4.png'
+import Image5a from '../assets/step5.1.png'
+import Image5b from '../assets/step5.2.png'
+import Image6 from '../assets/step6.png'
+import Image7 from '../assets/step7.png'
+import Image8 from '../assets/step8.png'
+import Image9 from '../assets/step9.jpg'
+import Image10 from '../assets/step10.png'
+import Image11 from '../assets/step11.png'
+import Image12 from '../assets/Step12.png'
+import Image13 from '../assets/lasstep.jpg'
 
 
 const Body = () => {
   return (
-    <div>Body</div>
-  )
-}
+    <div className='bg-white mt-4 mx-[4rem] text-black'>
+      <div className='flex flex-col sm:flex-row'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image1} alt='step1' className='max-w-full h-auto' />
+        </div>
+      </div>
+      
+      {/* Repeat for the second set */}
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: More text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image2} alt='step2' className='max-w-full h-auto' />
+        </div>
+      </div>
+      
+      {/* Repeat for the third set */}
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image3} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
 
-export default Body
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image4} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image5a} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image5b} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image6} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image7} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image8} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image9} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image10} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image11} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image12} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+
+      <div className='flex flex-col sm:flex-row mt-4'>
+        {/* Column 1: Text */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <p>Column 1: Additional text content goes here.</p>
+        </div>
+        
+        {/* Column 2: Image */}
+        <div className='w-full sm:w-1/2 p-4'>
+          <img src={Image13} alt='step3' className='max-w-full h-auto' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
