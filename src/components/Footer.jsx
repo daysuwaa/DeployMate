d659add352097d56cbcabe953064fb149306b727
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-22 20:17:54 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='mt-3' >
+    <div className='bg-gray-300 text-gray-700 text-xs h-[40px] pt-3 pl-5 block'>
+    <p>&copy; 2023 Adesuwa</p>
+     </div>
+    </div>
   )
 }
 
