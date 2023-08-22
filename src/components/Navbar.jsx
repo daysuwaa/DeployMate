a3dfb8ff2afc9d88364866e14ad4b72805195382
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-22 20:40:34 +0100

    fixed an error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div>
       <h1 className='bg-gray-200 text-center h-[5rem] md:pt-5 lg:pt-5 pt-3 text-black font-customFont text-2xl'>
-        How to Commit & Deploy Static Web pages to Git (Beginners Guide) </h1>
+        How to Commit & Deploy a Static Web pages to Git (Beginners Guide) </h1>
     </div>
   )
 }
